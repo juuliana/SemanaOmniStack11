2be00f8361c684127a80f063d8781ddfe1417e44
Juliana Ribeiro <jumaju10@hotmail.com>
2020-06-07 17:05:31 -0300

    Be The Hero

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll(async()=>{
+        await connection.destroy();
+    });
+
+    it('should be able create a new ONG', async() => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name: "APAD2",
+                email: "contato@fda.com",
+                whatsapp: "4700000000",
+                city: "Rio do Sul",
+                uf: "SC"
+            });
+
+            expect(response.body).toHaveProperty('id');
+            expect(response.body.id).toHaveLength(8);    
+        });
+});
